4d4647adde0bf2bf2f7d6a6207336e45cbb0e09f
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-04 21:29:05 +0200

    create & mount routers

--- app.js
+++ app.js
@@ -131,19 +131,17 @@ const deleteUSer = (req, res) => {
 };
 
 //routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUSer);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUSer);
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 //listening
 const port = 3000;
 app.listen(port, () => {
