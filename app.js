a86cd987188f1dee5b0a4093c544dbfcb8102a7f
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-04 17:05:07 +0200

    add update tour

--- app.js
+++ app.js
@@ -39,6 +39,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+//handel update tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+  //transform string to number
+  const id = req.params.id * 1;
+  if (id > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here ...>',
+    },
+  });
+});
 //handle post a new tour req
 app.post('/api/v1/tours', (req, res) => {
   //prepare the tour that will be added
