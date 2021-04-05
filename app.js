974b4f84e2ccf3410445cd004ee815f225665576
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-05 11:10:05 +0200

    environment variables

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 //middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
