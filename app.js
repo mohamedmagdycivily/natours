b080f15519f0ff356d24eae6e5ff57413641ea2a
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-21 15:42:35 +0200

    handling unhandled requests

--- app.js
+++ app.js
@@ -21,4 +21,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//handling wrong requests
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `there is no ${req.originalUrl} on this server `,
+  });
+});
+
 module.exports = app;
