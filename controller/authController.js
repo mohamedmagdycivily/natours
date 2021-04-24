16efe82455b8c07333aad6418aba8e7f8e95af8b
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-24 17:29:28 +0200

    login

--- controller/authController.js
+++ controller/authController.js
@@ -19,3 +19,25 @@ exports.signup = catchAsync(async (req, res, next) => {
     },
   });
 });
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if ((!email, !password)) {
+    return next(new AppError('please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  console.log(user);
+  // const correct = await user.correctPassword(password, user.password);
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+  });
+});
