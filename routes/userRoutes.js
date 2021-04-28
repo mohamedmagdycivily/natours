cd58b17d4f16cee3e2879f97c2638b40320b1fb8
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-28 04:00:27 +0200

    sending email

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.post('/forgetPassword', authController.forgetPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 router
   .route('/')
