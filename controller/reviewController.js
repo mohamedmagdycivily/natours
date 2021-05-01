afa8374cc888979d33601d6c0d226a39cfacfdae
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-05-01 02:38:38 +0200

    Factory Functions Update and Create

--- controller/reviewController.js
+++ controller/reviewController.js
@@ -16,19 +16,24 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+// exports.createReview = catchAsync(async (req, res, next) => {
+//   const newReview = await Review.create(req.body);
+
+//   res.status(201).json({
+//     status: 'success',
+//     data: {
+//       review: newReview,
+//     },
+//   });
+// });
+
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id; // comming from the protect middle ware
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  });
-});
-
+exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
